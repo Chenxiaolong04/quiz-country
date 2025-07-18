fc5ec00724a9c33fe28b6e0b3e3ce4031a7a55df
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-07-18 11:27:24 +0200

    aggiunto un pulsante per tornare alla home

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\QuizController;
 use App\Http\Controllers\FlagQuizController;
 use Illuminate\Support\Facades\Log;
 
-Route::get('/', [QuizController::class, 'home']);
+Route::get('/', [QuizController::class, 'home'])->name('home');
 Route::get('/quiz', [QuizController::class, 'quiz'])->name('quiz.show');
 Route::post('/quiz/check', [QuizController::class, 'checkAnswer'])->name('quiz.check');
 Route::get('/quiz/end', [QuizController::class, 'end'])->name('quiz.end');
